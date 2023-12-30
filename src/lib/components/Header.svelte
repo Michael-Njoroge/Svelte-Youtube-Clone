8b44cc97f40f44dcb09f4a08326473dc6d089a3b
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 10:59:57 +0300

    complete the search form component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,22 @@
+<script>
+
+	import SearchBar from "./SearchBar.svelte";
+
+</script>
  
 <header class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0">
+
     <div class="items-center flex gap-4">
        <img src="/icons/bugerIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
        <img src="/icons/ytLogo.svg" alt="">
     </div>
-    <div>Searchbar</div>
+
+    <SearchBar/>
+
     <div class="flex items-center gap-3">
         <img src="/icons/createIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
         <img src="/icons/notIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
         <img src="/avatar.png" class="bg-gray-200 h-8 w-8 rounded-full cursor-pointer" alt="">
     </div>
+    
 </header>
---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,6 @@
+<form class="flex h-[40px] basis-[600px]">
+    <input type="text" class="h-[40px] outline-none border rounded-s-3xl focus:border-blue-600 focus:border-2 px-2 flex-1 border-gray-400">
+    <button class="flex items-center justify-center cursor-pointer w-16 bg-slate-50 border rounded-e-3xl">
+        <img src="/icons/searchIcon.svg" alt="">
+    </button>
+</form>
