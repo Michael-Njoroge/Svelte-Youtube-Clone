c4c032045ff5f69b7d70d2880797d4d6c3bf6bfe
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 11:27:59 +0300

    finish up on  header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,14 +6,19 @@
  
 <header class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0">
 
-    <div class="items-center flex gap-4">
+    <!-- <a href="/">
+        <img src="/icons/ytLogo.svg" class="md:hidden mr-5" alt="">
+    </a> -->
+    <div class="md:flex hidden items-center gap-4">
        <img src="/icons/bugerIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
-       <img src="/icons/ytLogo.svg" alt="">
+    <a href="/">
+        <img src="/icons/ytLogo.svg" alt="">
+    </a>
     </div>
 
     <SearchBar/>
 
-    <div class="flex items-center gap-3">
+    <div class="md:flex hidden items-center gap-3">
         <img src="/icons/createIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
         <img src="/icons/notIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
         <img src="/avatar.png" class="bg-gray-200 h-8 w-8 rounded-full cursor-pointer" alt="">
