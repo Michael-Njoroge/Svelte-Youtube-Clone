658b51a0aa6009a78d4fb21237c485689f289794
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 20:09:15 +0300

    create video dynamic route

---
+++ src/routes/[videoId]/+page.svelte
@@ -0,0 +1,19 @@
+ <script lang="ts">
+    let videoId = ''
+ </script>
+
+ <!-- <iframe
+ class="w-full h-[480px]"
+ src={`https://www.youtube.com.embed/${videoId}`}
+ title="Youtube Video Player"
+ frameborder = "0"
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+ allowfullscreen
+ /> -->
+
+ <div class="max-w-[1150px] px-2 w-full mx-auto">
+    <h1 class="text-xl capitalize font-semibold">
+        svelte
+    </h1>
+    <div class="flex flex-col md:flex-row gap-5 items-start"></div>
+ </div>
