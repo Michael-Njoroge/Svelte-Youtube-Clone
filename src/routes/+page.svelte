d992fa528485b83c77f383baec4a54adf80caf53
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 10:41:30 +0300

    complete the header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl font-bold underline">
+<!-- <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
   
@@ -6,4 +6,4 @@
     :global(html) {
       background-color: theme(colors.gray.100);
     }
-  </style>
+  </style> -->
