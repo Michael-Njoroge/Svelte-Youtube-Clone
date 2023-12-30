5507504e761c9e7cb02f34741452dd2a8bdf63ea
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 14:18:29 +0300

    complete the videocard component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,11 +4,11 @@
 
 </script>
  
-<header class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0">
+<header class="flex border px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0">
 
-    <!-- <a href="/">
-        <img src="/icons/ytLogo.svg" class="md:hidden mr-5" alt="">
-    </a> -->
+    <a href="/"  class="md:hidden mr-5">
+        <img src="/icons/ytLogo.svg" alt="">
+    </a>
     <div class="md:flex hidden items-center gap-4">
        <img src="/icons/bugerIcon.svg" class="cursor-pointer p-3 rounded-full hover:bg-gray-200" alt="">
     <a href="/">
---
+++ src/lib/components/VideoCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<a href="/">
+    <div class="relative">
+    <div class="absolute bg-black/80 text-white font-semibold rounded-md bottom-1 right-1 text-xs px-1">10:20</div>
+    <img src="/thumbnail.webp" class="sm:[208px] h-[250px] w-full object-cover rounded-2xl bg-gray-200" alt="">
+    </div>
+    <div class="flex mt-3 gap-3">
+        <img src="/avatar.png" class="h-9 w-9 rounded-full bg-gray-100" alt="">
+        <div>
+            <h3 class="capitalize font-medium">React best</h3>
+            <p class="text-gray-600 capitalize text-sm mt-1">lawel</p>
+            <div class="text-gray-600 text-sm">
+                <span>
+                <p>2 mil</p>
+                </span>
+                
+                <span>
+                <p>2hrs ago</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</a>
