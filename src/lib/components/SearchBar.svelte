d73f9559a4d150b432a08a751e38523afd0336cf
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-31 13:11:35 +0300

    search bar  functionality

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,5 +1,11 @@
-<form class="flex h-[40px] basis-[600px]">
-    <input type="text" class="h-[40px] outline-none border rounded-s-3xl focus:border-blue-600 focus:border-2 px-2 flex-1 border-gray-400">
+<script>
+
+	import { enhance } from "$app/forms";
+	import { page } from "$app/stores";
+
+</script>
+<form action="/?/search" use:enhance method="POST" class="flex h-[40px] basis-[600px]">
+    <input type="text" class="h-[40px] outline-none border rounded-s-3xl focus:border-blue-600 focus:border-2 px-2 flex-1 border-gray-400" class:border-red-500={$page.form?.error} name="q" placeholder="Search...">
     <button class="flex items-center justify-center cursor-pointer w-16 bg-slate-50 border rounded-e-3xl">
         <img src="/icons/searchIcon.svg" alt="">
     </button>
--- src/routes/(groupLayout)/+page.server.ts
+++ src/routes/(groupLayout)/+page.server.ts
@@ -1,7 +1,21 @@
 import { getHomepage } from "$lib/services";
+import { fail } from "@sveltejs/kit";
 
 export const load = async () => {
     return {
         contents: await getHomepage()
     };
+};
+
+export const actions = {
+    search:async ({request}) => {
+        const data = await request.formData()
+        const query = data.get('q')
+        if(!query){
+            return fail(400,{
+                error: 'You have an empty search'
+            })
+        }
+        console.log('called')
+    }
 };
