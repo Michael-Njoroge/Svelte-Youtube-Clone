1560b598ff3bd0bcb88119e3fa2cba549e0460cb
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-31 08:03:03 +0300

    create the recommended videos component

---
+++ src/lib/components/RecommendedVideos.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<div class="space-y-3 basis-[360px]">
+
+</div>
--- src/routes/[videoId]/+page.svelte
+++ src/routes/[videoId]/+page.svelte
@@ -1,6 +1,7 @@
  <script lang="ts">
 	import ChannelInfo from "$lib/components/ChannelInfo.svelte";
 	import Comments from "$lib/components/Comments.svelte";
+	import RecommendedVideos from "$lib/components/RecommendedVideos.svelte";
 	import VideoDescription from "$lib/components/VideoDescription.svelte";
 
     let videoId = ''
@@ -24,4 +25,5 @@
         <VideoDescription/>
         <Comments/>
     </div>
+    <RecommendedVideos/>
  </div>
