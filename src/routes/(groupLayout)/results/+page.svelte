1dbdf4368066280dd3698c9cd0a9c965fd2058c0
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-31 15:12:36 +0300

    complete the search video functionality

--- src/routes/(groupLayout)/results/+page.svelte
+++ src/routes/(groupLayout)/results/+page.svelte
@@ -10,6 +10,13 @@
 </div>
 
 <div class="space-y-5">
+    {#each data.contents.contents as {type,channel,video}}
+
+    {#if type === 'video'}
+    <SearchVideoCard avatar = {video.author.avatar[0].url} badges = {video.badges} views = {video.stats.views} channelName = {video.author.title} descriptionSnippet = {video.descriptionSnippet} publishedTimeText = {video.publishedTimeText} thumbnail = {video.thumbnails[0].url} title = {video.title} videoId = {video.videoId}/>
+    {:else if type === 'channel'}
     <ChannelCard/>
-    <SearchVideoCard/>
+    {/if}
+
+    {/each}
 </div>
