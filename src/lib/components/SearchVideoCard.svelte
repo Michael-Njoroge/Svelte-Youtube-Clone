e0ba626c1df1bbb2da5888899971e63776dc1582
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-31 14:55:08 +0300

    complete the working of search props

--- src/lib/components/SearchVideoCard.svelte
+++ src/lib/components/SearchVideoCard.svelte
@@ -1,17 +1,29 @@
-<div class="flex gap-4">
-    <img src="/thumbnail.webp" class="max-w-[360px] object-cover aspect-video rounded-2xl w-full " alt="">
+<script lang="ts">
+    export let title:string
+    export let avatar:string
+    export let publishedTimeText:string
+    export let descriptionSnippet:string
+    export let thumbnail:string
+    export let channelName:string
+    export let videoId:string
+    export let badges:string[]
+    export let views:string[]
+</script>
+
+<a href="/{videoId}" class="flex gap-4">
+    <img src={thumbnail} class="max-w-[360px] object-cover aspect-video rounded-2xl w-full " alt="">
     <div class="text-gray-700">
-        <h1 class="capitalize font-medium text-lg text-black">python sucks</h1>
-        <p class="text-xs">10mil . 2hrs ago</p>
+        <h1 class="capitalize font-medium text-lg text-black">{title}</h1>
+        <p class="text-xs">{views}.{publishedTimeText}</p>
         <div class="flex items-center gap-2 my-4">
-            <img src="/avatar.png" class="w-6 h-6 rounded-full" alt="">
-            <p class="capitalize text-xs">mike</p>
+            <img src={avatar} class="w-6 h-6 rounded-full" alt="">
+            <p class="capitalize text-xs">{channelName}</p>
         </div>
-        <p class="text-xs">welcome to my channel</p>
+        <p class="text-xs">{descriptionSnippet}</p>
         <div class="flex items-center gap-2 mt-3">
-            <p class="bg-gray-200 text-gray-800 text-xs font-semibold py-0.5 px-1">4k</p>
-            <p class="bg-gray-200 text-gray-800 text-xs font-semibold py-0.5 px-1">4k</p>
-            <p class="bg-gray-200 text-gray-800 text-xs font-semibold py-0.5 px-1">4k</p>
+            {#each badges as badge}
+            <p class="bg-gray-200 text-gray-800 text-xs font-semibold py-0.5 px-1">{badge}</p>
+            {/each}
         </div>
     </div>
-</div>
+</a>
