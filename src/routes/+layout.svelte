69c51acbe41d042d93fb853e2290784308560f6d
Michael-Njoroge <mikethecoder12@gmail.com>
2024-01-01 11:50:30 +0300

    finale...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-	import Header from "$lib/components/Header.svelte";
-    import "./app.css"
+	import './app.css';
+	import Header from '$lib/components/Header.svelte';
+ 	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import NProgress from 'nprogress';
+	beforeNavigate(() => {
+		NProgress.start();
+	});
+	afterNavigate(() => {
+		NProgress.done();
+	});
+	NProgress.configure({
+		showSpinner: false
+	});
 </script>
 
-<Header/>
-<slot/>
- 
+<Header />
+
+<slot />
